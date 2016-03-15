ab70d134f5b6a4e602e14a3290fc1e2583abd87e
Kevin van Hoorn <kevin.van.hoorn@me.com>
2016-03-15 19:04:50 +0100

    - added uservariables for pir states (don't turn off when on by timer)

--- scripts/lua/script_device_pir.lua
+++ scripts/lua/script_device_pir.lua
@@ -13,14 +13,12 @@ local offMinutes = 3
 -- if PIR attic is on and it's night, turn on attic lights
 if c['PIR Zolder'] == 'On' and t['Nighttime'] == true then
 
-    if d['Zolder - bank'] == 'Off' then
+    if d['Zolder - bank'] == 'Off' or d['Zolder - scherm'] == 'Off' then
         commandArray['Zolder - bank'] = 'On'
-    end
-
-    if d['Zolder - scherm'] == 'Off' then
         commandArray['Zolder - scherm'] = 'On'
+        commandArray['Variable:PIR_Zolder_state'] = '1'
     end
-
+    
 end
 
 -- if PIR hall is on ant it's night, turn on hall lights
@@ -28,25 +26,33 @@ if c['PIR Hal'] == 'On' and t['Nighttime'] == true then
 
     if d['Hal - LED bak'] == 'Off' then
         commandArray['Hal - LED bak'] = 'On'
+        commandArray['Variable:PIR_Hal_state'] = '1'
     end
 
 end
 
 -- if PIR attic isn't seen for x minutes turn lights back off
 local difference = time_difference(dl['PIR Zolder'])
-if difference > (offMinutes * 60) and (d['Zolder - bank'] == "On" or d['Zolder - scherm'] == "On") then
+if difference > (offMinutes * 60) and u['PIR_Zolder_state'] == 1 then
     
     commandArray['Zolder - bank'] = 'Off'
     commandArray['Zolder - scherm'] = 'Off'
+    commandArray['Variable:PIR_Zolder_state'] = '0'
     
 end
 
 -- if PIR hall isn't seen for x minutes, turn lights back off
 local difference2 = time_difference(dl['PIR Hal'])
-if difference2 > (offMinutes * 60) and d['Hal - LED bak'] == "On" then
+if difference2 > (offMinutes * 60) and u['PIR_Hal_state'] == 1 then
 	
 	commandArray['Hal - LED bak'] = 'Off'
+    commandArray['Variable:PIR_Hal_state'] = '0'
 	
+end
+
+-- debug
+if next(commandArray) ~= nil then
+    vardump(commandArray)
 end
 
 return commandArray
