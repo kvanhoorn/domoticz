7977ffa3851d5c6030371f334c83384b96bb6c68
Kevin van Hoorn <kevin.van.hoorn@me.com>
2016-11-08 15:44:31 +0100

    - use own ip change detector external due to blocking by myip

--- scripts/lua/script_time_ipchange.lua
+++ scripts/lua/script_time_ipchange.lua
@@ -9,15 +9,15 @@ utils_time = (loadfile '/home/pi/domoticz/scripts/lua/utils/utils_time.lua')()
 
 commandArray = {}
 
-local handle = io.popen('/usr/bin/curl icanhazip.com')
+local handle = io.popen('/usr/bin/curl kvanhoorn.nl/myip/index.php')
 local result = handle:read()
 handle:close()
 
 if (ds['External IP'] ~= result) then
     
-    print('ip change')
+    print('ip change, now ' .. result .. ' was ' .. ds['External IP'])
     commandArray['UpdateDevice'] = '234|0|' .. result
-    commandArray['SendNotification'] = 'IP Change#' .. result .. ', was: ' .. ds['External IP']
+    commandArray['SendNotification'] = 'IPChange#now ' .. result .. ' was ' .. ds['External IP']
     
 end
 
