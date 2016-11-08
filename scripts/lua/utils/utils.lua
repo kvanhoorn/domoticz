26713eb481d70aea3a44398510a152196680a79c
Kevin van Hoorn <kevin.van.hoorn@me.com>
2016-11-08 15:43:28 +0100

    - added some more vars to utils about domoticz url and api

--- scripts/lua/utils/utils.lua
+++ scripts/lua/utils/utils.lua
@@ -1,8 +1,8 @@
 --[[
-    
+
     This file is included in every script
     it sets some short named variables and has some functions
-    
+
 ]]--
 
 t = timeofday
@@ -20,6 +20,11 @@ dl = otherdevices_lastupdate
 du = otherdevices_utility
 di = otherdevices_idx
 
+dom_ip    = '192.168.0.58'
+dom_port  = '8080'
+dom_full  = 'http://' .. dom_ip .. ':' .. dom_port
+dom_api   = dom_full .. '/json.htm?'
+
 -- the vardump for logfile (recursive print)
 function vardump(value)
   serpent = (loadfile '/home/pi/domoticz/scripts/lua/utils/serpent.lua')()
@@ -29,14 +34,14 @@ end
 
 -- return time difference till now in seconds
 function time_difference(s)
-   year = string.sub(s, 1, 4)
-   month = string.sub(s, 6, 7)
-   day = string.sub(s, 9, 10)
-   hour = string.sub(s, 12, 13)
-   minutes = string.sub(s, 15, 16)
-   seconds = string.sub(s, 18, 19)
+   td_year = string.sub(s, 1, 4)
+   td_month = string.sub(s, 6, 7)
+   td_day = string.sub(s, 9, 10)
+   td_hour = string.sub(s, 12, 13)
+   td_minutes = string.sub(s, 15, 16)
+   td_seconds = string.sub(s, 18, 19)
    t1 = os.time()
-   t2 = os.time{year=year, month=month, day=day, hour=hour, min=minutes, sec=seconds}
+   t2 = os.time{year=td_year, month=td_month, day=td_day, hour=td_hour, min=td_minutes, sec=td_seconds}
    difference = os.difftime (t1, t2)
    return difference
 end
