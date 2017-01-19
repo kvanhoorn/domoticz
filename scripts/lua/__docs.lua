e81f3e1fa469030a8256556bbdcbd6aa9d733469
Kevin van Hoorn <kevin.van.hoorn@me.com>
2017-01-19 20:15:10 +0100

    Added all sounds for pushover to docs

--- scripts/lua/__docs.lua
+++ scripts/lua/__docs.lua
@@ -67,6 +67,29 @@
                 0   Normal
                 1   High            - Bypass quiet hours
                 2   Emergency       - Confirmation
+            sound:
+                pushover - Pushover (default)
+                bike - Bike
+                bugle - Bugle
+                cashregister - Cash Register
+                classical - Classical
+                cosmic - Cosmic
+                falling - Falling
+                gamelan - Gamelan
+                incoming - Incoming
+                intermission - Intermission
+                magic - Magic
+                mechanical - Mechanical
+                pianobar - Piano Bar
+                siren - Siren
+                spacealarm - Space Alarm
+                tugboat - Tug Boat
+                alien - Alien Alarm (long)
+                climb - Climb (long)
+                persistent - Persistent (long)
+                echo - Pushover Echo (long)
+                updown - Up Down (long)
+                none - None (silent)                
 
         'SendEmail'             = 'subject#body#your@email.com'
         'SendSMS'               = 'subject'
